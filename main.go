cd1a004be904d435ed56bb895e2583114f6d1c98
rbell3 <m20066@g.metro-cit.ac.jp>
2024-01-16 15:42:57 +0900

    first commit

--- main.go
+++ main.go
@@ -22,6 +22,6 @@ func Hello() echo.HandlerFunc {
 
 func ApiHelloGet() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4417", "message": "Hello"})
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4417", "message": "Helloこんにちは"})
     }
 }
